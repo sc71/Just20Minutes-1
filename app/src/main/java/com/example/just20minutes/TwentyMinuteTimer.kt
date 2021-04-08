461d2c33ba6c0c8ae8e14d20649ce0383fc75749
Jack Bartlett <jackbbart@gmail.com>
2021-04-08 13:06:10 -0700

    added a new activity through the wisdom of sarah

---
+++ app/src/main/java/com/example/just20minutes/TwentyMinuteTimer.kt
@@ -0,0 +1,11 @@
+package com.example.just20minutes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TwentyMinuteTimer : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_twenty_minute_timer)
+    }
+}
