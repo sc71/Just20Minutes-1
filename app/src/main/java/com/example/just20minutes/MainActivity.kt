7236381911ce1fb37cca234d2a961db464c7ffd7
Dusty Fox <dustyjfox@icloud.com>
2021-04-13 12:25:08 -0700

    splash screen no timer cause rolo is stinky

--- app/src/main/java/com/example/just20minutes/MainActivity.kt
+++ app/src/main/java/com/example/just20minutes/MainActivity.kt
@@ -1,13 +1,19 @@
 package com.example.just20minutes
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //hi rolo
+        val goToTimerButton = findViewById<Button>(R.id.goToTimerButton)
+        goToTimerButton.setOnClickListener{
+            val intent = Intent(this,TwentyMinuteTimer::class.java)
+            startActivity(intent)
+        }
     }
 }
 
