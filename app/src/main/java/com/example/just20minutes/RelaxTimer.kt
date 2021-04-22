466df465f68abea78e91ab3d49c2a821ff540184
Jack Bartlett <jackbbart@gmail.com>
2021-04-22 13:00:46 -0700

    the 20 minute timer now connects to how did it go page, which connects to the 5 minute timer. we have to add the end stuff

--- app/src/main/java/com/example/just20minutes/RelaxTimer.kt
+++ app/src/main/java/com/example/just20minutes/RelaxTimer.kt
@@ -1,35 +1,31 @@
 package com.example.just20minutes
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
-import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import androidx.core.view.isVisible
 import java.util.*
 import java.util.concurrent.TimeUnit
 
-class TwentyMinuteTimer : AppCompatActivity() {
-
+class RelaxTimer : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_twenty_minute_timer)
+        setContentView(R.layout.activity_relax_timer)
 
-        val goToHowItGo = Intent(this,HowDidItGoButtons::class.java)
         var textView = findViewById<TextView>(R.id.text_view)
         val duration = TimeUnit.MINUTES.toMillis(1)
         var veinte = object : CountDownTimer(duration, 1000) {
             override fun onTick(l: Long) {
                 var sDuration = String.format(Locale.ENGLISH, "%02d : %02d",
                         TimeUnit.MILLISECONDS.toMinutes(l),
-                        TimeUnit.MILLISECONDS.toSeconds(l) - TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(1)))
+                        TimeUnit.MILLISECONDS.toSeconds(l) - TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(l)))
                 textView.text = sDuration
             }
 
             override fun onFinish() {
-                startActivity(goToHowItGo)
+                textView.isVisible = false
             }
         }
 
