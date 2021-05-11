5cc037b507e1d4b344f100a9ae7f053181b5d9ce
jackbbart <jackbbart@gmail.com>
2021-05-11 12:50:16 -0700

    ITS YOUR BIRTHDAY TODAY KIMMY X10000

--- app/src/main/java/com/example/just20minutes/ItemScreen.kt
+++ app/src/main/java/com/example/just20minutes/ItemScreen.kt
@@ -2,6 +2,7 @@ package com.example.just20minutes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class ItemScreen : AppCompatActivity() {
     companion object{
@@ -12,5 +13,10 @@ class ItemScreen : AppCompatActivity() {
         setContentView(R.layout.activity_item_screen)
 
         val toDo = intent.getParcelableExtra<ToDoItem>(EXTRA_TODO_ITEM)
+
+        val name = toDo?.name
+
+        var textView = findViewById<TextView>(R.id.itemName)
+        textView.text = name
     }
 }
