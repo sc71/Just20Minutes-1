04f8bb596ec588562daefc0a6845096c6d82f726
Kimberly K <kimberly.n.kubo@gmail.com>
2021-05-17 12:23:57 -0700

    added progressBar button, attempted to connect it to timer.

--- app/src/main/java/com/example/just20minutes/HowDidItGoButtons.kt
+++ app/src/main/java/com/example/just20minutes/HowDidItGoButtons.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.os.CountDownTimer
 import android.view.WindowManager
 import android.widget.Button
+import android.widget.ProgressBar
 import androidx.core.view.isVisible
 import java.util.*
 import java.util.concurrent.TimeUnit
@@ -16,7 +17,9 @@ class HowDidItGoButtons : AppCompatActivity() {
         setContentView(R.layout.activity_how_did_it_go_buttons)
         window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
 
-        val toRelaxTimerButton = findViewById<Button>(R.id.toRelaxTimerButton)
+        val progressBar = findViewById<ProgressBar>(R.id.progressBar_howDidItGo_timer)
+
+        val toRelaxTimerButton = findViewById<Button>(R.id.button_howDidItGo_timer)
         toRelaxTimerButton.setOnClickListener{
             val intent = Intent(this,RelaxTimer::class.java)
             startActivity(intent)
@@ -31,8 +34,13 @@ class HowDidItGoButtons : AppCompatActivity() {
         }
 
         val duration = TimeUnit.MINUTES.toMillis(1)
+        val startTime = 2000
+        var timeLeftInMillis = startTime
         var cinco = object : CountDownTimer(duration, 1000) {
-            override fun onTick(l: Long) {
+            override fun onTick(millisUntilFinished: Long) {
+                timeLeftInMillis = millisUntilFinished.toInt()
+                progressBar.progress = (millisUntilFinished/100).toInt()
+
                 //maybe we add the thing where the button becomes darker as we the time goes on
                 }
 
@@ -41,5 +49,6 @@ class HowDidItGoButtons : AppCompatActivity() {
                 finish()
             }
         }
+        cinco.start()
     }
 }
